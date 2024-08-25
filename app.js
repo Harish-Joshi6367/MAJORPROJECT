6e9d75d9a749f193099cb665a980a24f1db49ae6
Harish-Joshi6367 <Harisharma6367@gmail.com>
2024-08-25 13:26:58 +0530

    hello commit

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ app.use((req, res, next) => {
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
-app.use("/", iconRouter);
+app.use("/listing", iconRouter);
 
 
 app.listen(1000, ()=> {
